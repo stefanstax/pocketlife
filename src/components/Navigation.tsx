b92c73c6f833c77ee41bf35a5c68da794ab1ac66
Stefan Stäx <stefanstax@192.168.1.40>
2025-07-04 12:24:11 +0200

    Navigation rework WIP

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import type { RootState } from "../app/store";
-import { Link } from "react-router";
+import { Link, NavLink, useLocation } from "react-router";
 import LoginButton from "./LoginButton";
 import { SECONDARY, SHARED } from "../app/globalClasses";
 import LogoutButton from "./LogoutButton";
@@ -8,40 +8,59 @@ import { IoLockClosed } from "react-icons/io5";
 
 const Navigation = () => {
   const { user, token } = useSelector((state: RootState) => state.auth);
-  const currentURL = window.location.href;
+
+  const links = [
+    {
+      path: "/transactions",
+      visibleForGuest: true,
+      label: "Transactions",
+    },
+    {
+      path: "/select-currencies",
+      visibleForGuest: true,
+      label: "Toggle Currencies",
+    },
+    {
+      path: "/currencies",
+      visibleForGuest: false,
+      label: "Manage Currencies",
+    },
+    {
+      path: "/links",
+      visibleForGuest: false,
+      label: "Links",
+    },
+    {
+      path: "/storage",
+      visibleForGuest: false,
+      label: "Storage",
+    },
+  ];
 
   return (
     <nav className="w-full mx-auto p-4 bg-gray-800 lg:gap-4 flex flex-wrap items-center justify-between">
-      <h1 className="text-2xl text-gray-400 font-black">PocketLife</h1>
+      <h1 className="text-2xl text-gray-200 font-black">PocketLife</h1>
       <div className="navigation flex flex-wrap text-gray-300  items-center gap-4">
-        <Link
-          to="/transactions/"
-          className={`hover:text-[#5152fb] p-3 rounded-lg font-bold`}
-        >
-          Transactions
-        </Link>
-        {user?.email === import.meta.env.VITE_ADMIN_EMAIL && (
-          <Link
-            to="/currencies/"
-            className=" hover:text-[#5152fb] p-3 rounded-lg font-bold"
-          >
-            Manage Currencies
-          </Link>
-        )}
-        <Link
-          to="/select-currencies/"
-          className=" hover:text-[#5152fb] p-3 rounded-lg font-bold"
-        >
-          Toggle Currencies
-        </Link>
-        <Link to="#" className=" flex items-center gap-2  opacity-50 font-bold">
-          <IoLockClosed />
-          Links
-        </Link>
-        <Link to="#" className="flex gap-2 items-center  opacity-50 font-bold">
-          <IoLockClosed />
-          Storage
-        </Link>
+        {links.map((link) => {
+          return (
+            <NavLink
+              key={link?.path}
+              to={link?.path}
+              className={({ isActive }) =>
+                `flex gap-2 items-center ${
+                  isActive
+                    ? "text-blue-600"
+                    : !link?.visibleForGuest
+                    ? "opacity-50"
+                    : "text-gray-200"
+                }`
+              }
+            >
+              {!link?.visibleForGuest && <IoLockClosed />}
+              {link?.label}
+            </NavLink>
+          );
+        })}
       </div>
       <div className="flex flex-wrap gap-2">
         {!token && (
